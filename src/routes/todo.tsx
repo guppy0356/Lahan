3ac0b30ec6af97e3a6b76841d62e21bc09d3c2e9
guppy0356 <guppy0356.nakira@gmail.com>
2026-02-12 23:59:09 +0900

    Add error component to /todo route for API error handling

--- src/routes/todo.tsx
+++ src/routes/todo.tsx
@@ -1,10 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router'
+import type { ErrorComponentProps } from '@tanstack/react-router'
 import { Suspense } from 'react'
+import { ApiError } from '../api/client'
 import { useTodoFacade } from '../features/todo/Todo.facade'
 import { TodoView } from '../features/todo/Todo.component'
 
+function TodoError({ error, reset }: ErrorComponentProps) {
+  return (
+    <div className="p-8 text-center">
+      <p className="text-red-600 font-semibold">
+        {error instanceof ApiError
+          ? `Error ${error.status}: ${error.message}`
+          : error.message}
+      </p>
+      <button
+        onClick={reset}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
+        Retry
+      </button>
+    </div>
+  );
+}
+
 export const Route = createFileRoute('/todo')({
   component: TodoPage,
+  errorComponent: TodoError,
 })
 
 function TodoSkeleton() {
